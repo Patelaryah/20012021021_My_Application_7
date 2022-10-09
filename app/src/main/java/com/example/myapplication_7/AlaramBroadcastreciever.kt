059811a2c78e4ac516038face1f0456508b2ead6
Patelaryah <patelarya20@gnu.ac.in>
2022-10-09 17:23:01 +0530

    Initial commit

---
+++ app/src/main/java/com/example/myapplication_7/AlaramBroadcastreciever.kt
@@ -0,0 +1,27 @@
+package com.example.myapplication_7
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+
+class AlaramBroadcastreciever : BroadcastReceiver()
+{
+
+    override fun onReceive(context: Context, intent: Intent)
+    {
+        val str1 = intent.getStringExtra("Service1")
+        if(str1 == "Start" || str1 == "Stop")
+        {
+            val intentService = Intent(context, AlaramService::class.java)
+            intentService.putExtra("Service1", intent.getStringExtra("Service1"))
+            if(str1 == "Start")
+            {
+                context.startService(intentService)
+            }
+        else if(str1=="Stop")
+        {
+            context.stopService(intentService)
+        }
+        }
+    }
+}
---
+++ app/src/main/java/com/example/myapplication_7/AlaramService.kt
@@ -0,0 +1,29 @@
+package com.example.myapplication_7
+
+import android.app.Service
+import android.content.Intent
+import android.media.MediaPlayer
+import android.os.IBinder
+
+class AlaramService : Service()
+{
+    var mp: MediaPlayer? = null
+    override fun onBind(intent: Intent): IBinder
+    {
+        TODO("Return the communication channel to the service.")
+    }
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int
+    {
+        if(intent != null)
+        {
+            mp = MediaPlayer.create(this, R.raw.alaram);
+            mp?.start()
+        }
+        return START_STICKY
+    }
+    override fun onDestroy()
+    {
+        mp?.stop()
+        super.onDestroy()
+    }
+}
